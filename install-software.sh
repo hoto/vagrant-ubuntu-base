023b869d627107dd382f399851afb45e4f329ffc
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-05-22 13:17:12 +0100

    Install docker compose

--- install-software.sh
+++ install-software.sh
@@ -5,10 +5,20 @@ sudo apt -y update
 sudo apt install -y tree 
 
 function installDocker() {
-  echo "Installing Docker CE..."
-  curl -fsSL get.docker.com -o /usr/local/src/get-docker.sh
-  sudo sh /usr/local/src/get-docker.sh
-  sudo usermod -aG docker vagrant
+  if ! [ -x "$(command -v docker)" ]; then
+    echo "Installing Docker CE..."
+    curl -fsSL get.docker.com -o /usr/local/src/get-docker.sh
+    sudo sh /usr/local/src/get-docker.sh
+    sudo usermod -aG docker vagrant
+  fi
+}
+
+function installDockerCompose() {
+  echo "Installing Docker Compose..."
+  sudo curl \
+    -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) \
+    -o /usr/local/bin/docker-compose
 }
 
 installDocker
+installDockerCompose
